33048f8d1efbe29fd213679133e441767ccd5c78
Wayne Potts <waynepotts@outlook.dk>
2026-05-01 16:20:08 +0200

    frontend initial commit

---
+++ frontend/crypto_front/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.tsx'
+import CryptoChart from './linechart.tsx'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <CryptoChart />
+    </StrictMode>
+ /* <StrictMode>
+    <App />
+  </StrictMode>*/
+
+)
